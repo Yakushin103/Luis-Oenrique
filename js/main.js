e95ddf571d9fba7f6b3d87058f4cb5e6a4c9c0e4
Yakushin103 <46379749+Yakushin103@users.noreply.github.com>
2019-01-18 16:44:12 +0300

    Add slide and new section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
-
-//var elems = document.querySelectorAll('.menu__link');
-//
-//
-//for (var item = 0; item < elems.length; item++){
-//
-//  elems[item].addEventListener('click', function(event) {
-//    event.target.classList.toggle('menu__link--active');
-//  });
-//}
+// menu
 
 var elements = document.querySelectorAll('.menu__link');
 for (var i = 0; i < elements.length; i++) {
@@ -25,3 +16,32 @@ function removeClass(){
     elements[i].classList.remove('menu__link--active');
   }
 }
+
+
+// grafic
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales'],
+          ['2004',  83],
+          ['',  58],
+          ['2005',  73],
+          ['',  55],
+          ['2006',  95],
+          ['',  55],
+          ['2007',  80],
+          ['',  40]
+        ]);
+
+        var options = {
+          curveType: 'function',
+          legend: { position: 'none' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
